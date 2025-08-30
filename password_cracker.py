d4e5697a1aba6b72c7b32bfbf2f5536021ccfd50
MauriCarbon <mauricio.carbon2004@gmail.com>
2025-08-29 22:00:34 -0300

    update password_cracker.py

--- password_cracker.py
+++ password_cracker.py
@@ -2,7 +2,7 @@ import hashlib
 
 def crack_sha1_hash(hash_to_crack, use_salts=False):
     # Abrimos la lista de passwords
-    with open("./SHA-1_password_cracker/top-10000-passwords.txt", "r", encoding="utf-8") as f:
+    with open("top-10000-passwords.txt", "r", encoding="utf-8") as f:
         passwords = [line.strip() for line in f]
 
     if not use_salts:
@@ -13,7 +13,7 @@ def crack_sha1_hash(hash_to_crack, use_salts=False):
                 return password
     else:
         # Cargamos las salts
-        with open("./SHA-1_password_cracker/known-salts.txt", "r", encoding="utf-8") as f:
+        with open("known-salts.txt", "r", encoding="utf-8") as f:
             salts = [line.strip() for line in f]
 
         # Probamos todas las combinaciones salt+password y password+salt
@@ -31,5 +31,5 @@ def crack_sha1_hash(hash_to_crack, use_salts=False):
 
     return "PASSWORD NOT IN DATABASE"
 
-print(crack_sha1_hash("5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"))
+#print(crack_sha1_hash("5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")) #returns password
 #print(crack_sha1_hash("53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)) # returns superman
